96322ace36eefeacde1d0c82eebbbe941c23ed60
Marc Friedenberg <maf946@psu.edu>
2021-11-08 15:17:47 -0500

    First draft

--- IPFragCalc.py
+++ IPFragCalc.py
@@ -1,6 +1,9 @@
 import argparse
+from argparse import RawTextHelpFormatter
 
-parser = argparse.ArgumentParser(description='Calculate relevant IP fragmentation values.')
+usageInfo = "Example usage:\npython3 IPFragCalc.py --dataSize 5144 --MTU 1500 --fragIdentifier y --headerSize 20"
+
+parser = argparse.ArgumentParser(description='A teaching tool for IP fragmentation', epilog=usageInfo, formatter_class=RawTextHelpFormatter)
 parser.add_argument('--dataSize', dest='dataSize', type=int, nargs='?', default=4000,
                     help='The size of the datagram to be formatted')
 parser.add_argument('--MTU', dest='MTU', type=int, nargs='?', default=1500, help='The maximum transmission unit')
@@ -8,7 +11,6 @@ parser.add_argument('--fragIdentifier', dest='fragIdentifier', nargs='?', defaul
 parser.add_argument('--headerSize', dest='headerSize', type=int, nargs='?', default=20,
                     help='The size of the IP header. This amount is deducted from the dataSize when calculating '
                          'fragment lengths')
-
 args = parser.parse_args()
 
 dataSize = args.dataSize
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # IPFragCalculator
 A teaching tool for IP fragmentation
+
+## Usage (for IST 220 students)
+
+Download the file `IPFragCalc.py` to your virtual machine. Open the Terminal, `cd` to the folder where the file downloaded, and then run `python3 IPFragCalc.py`. 
+
+When you run the program, you can choose to assign values for the datagram length, the MTU, the fragment identifier, and the header size. By default, the following values are used:
+
+|            Argument | Default Value |
+|--------------------:|:-------------:|
+|     Datagram length | 4000          |
+|                 MTU | 1500          |
+| Fragment idenfifier | x             |
+|         Header size | 20            |
+
+To see how to customize these values, run `python3 IPFragCalc.py --help`.
